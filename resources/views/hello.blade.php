8d364d717f13950c30551b12f396a53b45f6b556
Haris Rachman <hrrachman28@gmail.com>
2024-06-08 10:43:37 +0700

    Task:Hello-World

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<style>
+        body {
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: 'Great Vibes', cursive;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        p {
+            font-size: 72px;
+            font-weight: bold;
+            color: royalblue;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+<body>
+	<div class="container">
+		<p>Hello World!</p>
+	</div>
+</body>
+</html>
